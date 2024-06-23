b74674a2a7d7b7029609682b65fe7e26251e6b79
3shigami <115579067+3shigami@users.noreply.github.com>
2024-06-23 18:30:19 +0300

    Update card.py

--- nrl/messages/pay_json/card.py
+++ nrl/messages/pay_json/card.py
@@ -9,3 +9,11 @@ class Card:
             data = json.load(f)
         return data["card"]
     
+    def change_card(card):
+        with open("messages/pay_json/json_files/card.json", "r", encoding="UTF-8") as f:
+            data = json.load(f)
+
+        data["card"] = card
+        with open("logs/data.json", "w", encoding="UTF-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    
